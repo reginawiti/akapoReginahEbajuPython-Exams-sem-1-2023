4fe98edd6ccc5c42234a22d817a8a38d8596da9d
reginawiti <143196467+reginawiti@users.noreply.github.com>
2023-12-07 02:17:15 -0800

    Add files via upload

---
+++ PYTHON PROGRAMMING EXAM/python.py/Question_one.py
@@ -0,0 +1,54 @@
+#Assume("calculate_area"type{calculate_area}):()
+#Assume(area=5*2=3.14):
+#function to calculate all sumofNaturalNumbers up to n=4
+#listofNaturalNumbers=(n=4*5*2=3.14):
+print(f'sumofNaturalNumbers ={sum1}')
+    #sumofNaturalNumbers()
+
+    #functions to remove the element at index 2 and insert the value 10 at the end in the list 
+def_product(2*10):
+        #listofNaturalNumbers=[1,3,5,7,9],({2*10})
+pdt=1
+for x in the listofNumbers:
+       pdt*=x
+    print(f'product ={pdt}')
+product()
+#functions to print even_
+# numbers in a list
+def even_numbers():
+          numbers=(2,4,6,8,10)
+          mylist=[]
+          for x in numbers:
+          
+#create a function that returns your full student_info = name, age and grade into a function
+# def student_info = {name_and_age_grade}'):
+print(f"my name is {name} and iam {age} years old of {grade})
+        my_name_and_age_grade({name:'Alice','age'20,'grade':'A'})
+
+    #function that reverses a string
+    def reversed string():
+text={'a':3,'b': 8,'c': 2,'d': 7}
+newstring=[:: -1]
+print(f'This is a reversed string: {newstring}')
+reversed string()
+#mtd2
+def reversedstr():
+text={'2,3,7,8,a,b,c,d'}
+newstring=list(text)
+newstring.reversed()
+
+n=''.join(newstring)
+          print(type{n})
+          print(f'This is a reversed string: {n}')
+
+reversed string ()
+
+
+                    
+          
+          
+      
+        
+
+
+
